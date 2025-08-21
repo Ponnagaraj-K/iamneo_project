aa9f8ec5b1e49a4377fb986c640d907ffbc9a393
Ponnagaraj-K <727723euci036@skcat.ac.in>
2025-08-21 22:13:28 +0530

    Fix Jest compilation - rename .jsx to .js files

---
+++ reactapp/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 Faster Bike Taxi. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
