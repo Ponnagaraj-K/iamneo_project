45eb1f432a5af87dbdee483bc395a341cba2a267
Ponnagaraj-K <727723euci036@skcat.ac.in>
2025-08-22 20:55:51 +0530

    Revert to working localhost configuration

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081", "https://ide-cdaebefafaeedecddaabafefccfecdeabcadecaab.premiumproject.examly.io")
+                        .allowedOrigins("http://localhost:8081")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
