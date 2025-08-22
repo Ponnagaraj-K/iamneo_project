8db8fbe83d25ac7ace621dab957e180ff59dd608
Ponnagaraj-K <727723euci036@skcat.ac.in>
2025-08-22 21:32:13 +0530

    Use direct port URLs for portal backend connection

--- reactapp/src/components/DisplayBikes.jsx
+++ reactapp/src/components/DisplayBikes.jsx
@@ -11,7 +11,7 @@ const DisplayBikes = () => {
 
   const fetchBiketaxis = async () => {
     try {
-      const response = await fetch('/proxy/8080/getAllBiketaxi', {
+      const response = await fetch('https://8080-cdaebefafaeedecddaabafefccfecdeabcadecaab.premiumproject.examly.io/getAllBiketaxi', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
