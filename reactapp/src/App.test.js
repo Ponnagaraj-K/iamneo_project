d5c94953a58c31fdc9f57795358f138f2ab51c55
Ponnagaraj-K <727723euci036@skcat.ac.in>
2025-08-23 15:26:28 +0530

    Revert to original test structure

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -11,6 +11,7 @@ import ApplyForm from "./components/ApplyForm";
 test("renders_home_component_with_title_and_description", () => {
   render(<App />);
 
+  // Check if the title and description are rendered
   const titleElement = screen.getByText("Welcome to Faster Bike Taxi");
   const descriptionElement = screen.getByText(
     "Apply now to become a bike taxi driver and start earning!"
@@ -27,16 +28,17 @@ test("renders_apply_now_button_with_link_to_apply", () => {
     </MemoryRouter>
   );
 
+  // Check if the "Apply Now" button is rendered with the correct link
   const applyButton = screen.getByText("Apply Now");
-  const linkElement = applyButton.closest('a');
 
   expect(applyButton).toBeInTheDocument();
-  expect(linkElement).toHaveAttribute("href", "/apply");
+  expect(applyButton.closest('a')).toHaveAttribute("href", "/apply");
 });
 
 test("renders_navbar_component_with_links", () => {
   render(<App />);
 
+  // Check if the component renders the title and links
   const titleElement = screen.getByText("Faster Bike Taxi");
   const homeLink = screen.getByText("Home");
   const bikeDetailsLink = screen.getByText("Bike Details");
@@ -53,6 +55,7 @@ test("checks_link_destinations", () => {
     </MemoryRouter>
   );
 
+  // Check if the links have the correct destinations
   const homeLink = screen.getByText("Home");
   const bikeDetailsLink = screen.getByText("Bike Details");
 
@@ -63,6 +66,7 @@ test("checks_link_destinations", () => {
 test("renders_footer_component_with_copyright_text", () => {
   render(<Footer />);
 
+  // Check if the copyright text is rendered
   const copyrightText = screen.getByText(
     /Faster Bike Taxi. All rights reserved./i
   );
@@ -71,6 +75,7 @@ test("renders_footer_component_with_copyright_text", () => {
 });
 
 test("fetching_and_displaying_bike_applications", async () => {
+  // Mocked data to simulate the response from the API
   const MOCK_DATA = [
     {
       name: "John Doe",
@@ -86,6 +91,7 @@ test("fetching_and_displaying_bike_applications", async () => {
     },
   ];
 
+  // Mock the fetch function to return the mocked data
   const fetchMock = jest.spyOn(global, "fetch").mockResolvedValue({
     ok: true,
     json: () => Promise.resolve(MOCK_DATA),
@@ -93,15 +99,20 @@ test("fetching_and_displaying_bike_applications", async () => {
 
   render(<DisplayBikes />);
 
-  await waitFor(() => {
-    MOCK_DATA.forEach((application) => {
-      expect(screen.getByText(application.name)).toBeInTheDocument();
-      expect(screen.getByText(application.bikeNumber)).toBeInTheDocument();
-      expect(screen.getByText(application.age.toString())).toBeInTheDocument();
-      expect(screen.getByText(application.phoneNumber)).toBeInTheDocument();
-    });
-  });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  // Check if each song is displayed in the table
+
+  await waitFor(() => {
+  MOCK_DATA.forEach((application) => {
+    expect(screen.getByText(application.name)).toBeInTheDocument();
+    expect(screen.getByText(application.bikeNumber)).toBeInTheDocument();
+    expect(screen.getByText(application.age)).toBeInTheDocument();
+    expect(screen.getByText(application.phoneNumber)).toBeInTheDocument();
+  });
+});
+
+  // Validate the fetch function call
   expect(fetchMock).toHaveBeenCalledWith(
     expect.stringContaining("/getAllBiketaxi"),
     expect.objectContaining({
@@ -114,11 +125,11 @@ test("fetching_and_displaying_bike_applications", async () => {
   fetchMock.mockRestore();
 });
 
+
 test("submits_valid_application_form", async () => {
-  const fetchMock = jest.spyOn(global, "fetch").mockResolvedValue({ ok: true });
-  
   render(<ApplyForm />);
 
+  // Fill in the form
   const nameInput = screen.getByLabelText("Name:");
   const bikenumberInput = screen.getByLabelText("Bike Number:");
   const ageInput = screen.getByLabelText("Age:");
@@ -126,20 +137,24 @@ test("submits_valid_application_form", async () => {
   const submitButton = screen.getByText("Submit Application");
 
   await act(async () => {
-    fireEvent.change(nameInput, { target: { value: "John Doe" } });
-    fireEvent.change(bikenumberInput, { target: { value: "B123" } });
-    fireEvent.change(ageInput, { target: { value: "25" } });
-    fireEvent.change(phonenumberInput, { target: { value: "1234567890" } });
-  });
+  fireEvent.change(nameInput, { target: { value: "John Doe" } });
+  fireEvent.change(bikenumberInput, { target: { value: "B123" } });
+  fireEvent.change(ageInput, { target: { value: "25" } });
+  fireEvent.change(phonenumberInput, { target: { value: "1234567890" } });
 
+  })
+  // Ensure that the form elements have the expected values
   expect(nameInput).toHaveValue("John Doe");
   expect(bikenumberInput).toHaveValue("B123");
-  expect(ageInput).toHaveValue(25);
+  expect(ageInput).toHaveValue(Number(25));
   expect(phonenumberInput).toHaveValue("1234567890");
+  const fetchMock = jest.spyOn(global, "fetch").mockResolvedValue({ ok: true });
 
+  // Submit the form
   await act(async () => {
-    fireEvent.click(submitButton);
-  });
+  fireEvent.click(submitButton);
+  })
+  expect(fetchMock).toHaveBeenCalledTimes(1);
 
   expect(fetchMock).toHaveBeenCalledWith(
     expect.stringContaining("/addBiketaxi"),
@@ -151,10 +166,13 @@ test("submits_valid_application_form", async () => {
       body: expect.any(String),
     })
   );
+  const button = screen.getByText("Submit Application");
+  expect(button).toBeInTheDocument();
+  fireEvent.click(button);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  await waitFor(() => {
-    expect(screen.getByText("Your application has been submitted successfully!")).toBeInTheDocument();
-  });
+  const bikeDetailsLink = screen.getByText("Your application has been submitted successfully!");
+  expect(bikeDetailsLink).toBeInTheDocument();
 
   fetchMock.mockRestore();
 });
@@ -162,9 +180,11 @@ test("submits_valid_application_form", async () => {
 test("submits_invalid_application_form", () => {
   render(<ApplyForm />);
 
+  // Fill in the form with invalid data
   const submitButton = screen.getByText("Submit Application");
-  fireEvent.click(submitButton);
+  fireEvent.click(submitButton); // Try to submit without filling in the form
 
+  // Check for validation error messages
   expect(screen.getByText("Name is required")).toBeInTheDocument();
   expect(screen.getByText("Bike Number is required")).toBeInTheDocument();
   expect(screen.getByText("Age is required")).toBeInTheDocument();
@@ -172,12 +192,11 @@ test("submits_invalid_application_form", () => {
 });
 
 test("checks_all_components_and_routes", () => {
+  
   render(<App />);
-  
-  const homeLink = screen.getByText(/Home/i);
-  fireEvent.click(homeLink);
+  const Home = screen.getByText(/Home/i);
+  fireEvent.click(Home);
   expect(screen.getByText("Welcome to Faster Bike Taxi")).toBeInTheDocument();
-  
   const applyLink = screen.getByText("Apply Now");
   expect(applyLink).toBeInTheDocument();
   fireEvent.click(applyLink);
